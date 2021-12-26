200d2dad9166720264935f9ce362a9a62e2805f9
Nishant-Goutam <nishu70555@gmail.com>
2021-12-26 20:12:50 +0530

    initial commit

---
+++ src/urlConfig.js
@@ -0,0 +1,7 @@
+const baseUrl = "https://ecart-backend-nishant.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
